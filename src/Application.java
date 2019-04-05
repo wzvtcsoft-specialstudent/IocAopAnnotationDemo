5449f2a8f1d1170efd94e64127c4b1858f7b5cbe
timshaw9791 <84539606@qq.com>
2019-04-05 19:21:43 +0800

    在Application中添加scanElements扫描包下的所有类，并输出所有类上面的注解，如果需要使用，可以自行修改或调试  注意，本方法只能扫描我们自己定义的类，而不能扫描jar里的类，如java.lang
     如果完整编写获取classpath下任意包的类，可以尝试这个：
     https://stackoverflow.com/questions/259140/scanning-java-annotations-at-runtime
     第一个关于spring的两个类。

--- src/Application.java
+++ src/Application.java
@@ -1,15 +1,23 @@
 import test.XController;
 
+import java.io.File;
+import java.lang.annotation.Annotation;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Application {
     public static void main(String[] args) {
        // XController xc=new XController();
-        XController xc=(XController)Application.getContext().get("XController");
+        scanElements("test");
+
+      /*  XController xc=(XController)Application.getContext().get("XController");
 
         int sum=xc.add(3,5);
         System.out.println(sum);
+        */
 
     }
 
@@ -22,4 +30,41 @@ public class Application {
         return null;
     }
 
+    /**
+     * 扫描包下的所有类，并输出所有类上面的注解，如果需要使用，可以自行修改或调试
+     * 注意，本方法只能扫描我们自己定义的类，而不能扫描jar里的类，如java.lang
+     * @param packageName
+     */
+     static public void scanElements(String packageName){
+        try {
+            //Load the classLoader which loads this class.
+            ClassLoader classLoader = Application.class.getClassLoader();
+
+            //Change the package structure to directory structure
+            String packagePath  = packageName.replace('.', '/');
+            URL urls = classLoader.getResource(packagePath);
+
+            //Get all the class files in the specified URL Path.
+            File folder = new File(urls.getPath());
+            File[] classes = folder.listFiles();
+
+            int size = classes.length;
+            List<Class<?>> classList = new ArrayList<Class<?>>();
+
+            for(int i=0;i<size;i++){
+                int index = classes[i].getName().indexOf(".");
+                String className = classes[i].getName().substring(0, index);
+                String classNamePath = packageName+"."+className;
+                Class<?> repoClass;
+                repoClass = Class.forName(classNamePath);
+                Annotation[] annotations = repoClass.getAnnotations();
+                for(int j =0;j<annotations.length;j++){
+                    System.out.println("Annotation in class "+repoClass.getName()+ " is "+annotations[j].annotationType().getName());
+                }
+                classList.add(repoClass);
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
